55f6efbb84ecf7ea1c284b827730966d052f8941
kaushtubh agrawal <kaush@MacBook-Air.local>
2024-08-18 15:43:33 +0530

    Added register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,41 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { User} from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.staus(200).json({
-        message: "ok"
+   // Get user details from frontend
+   // validation of data form == not empty
+   //check if user already exist:username, email
+   // check for images, check for avatar
+   // upload them to cloudinary , avatar
+   // create user object - create entry in db
+   // remove password and refresh token field from response
+   // check fro user creation 
+   // return response 
+
+   const {fullname, email, username, password} = req.body
+   console.log("email: ", email)
+
+//   ------------------- BEGINNER CHOICE-------------------------
+//    if(fullname === ""){
+//          throw new ApiError(400, "Fullname is required")
+//    }
+
+
+//    ------------------- INTERMEDIATE CHOICE-------------------------
+    if(
+        [fullname, email, username, password].some((field) => field?.trim() === "" )
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+    
+    const existedUser = User.findOne({
+        $or: [{ email }, { username }]
     })
+
+    if (existedUser) {
+        throw new ApiError(400, "User already exists")
+    }
 })
 
 export {registerUser}
--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
+// This function will handle the user registration logic.
 import { registerUser } from "../controllers/user.controller.js";
+// This middleware handles file uploads.
+import { upload } from "../middlewares/multer.middleware.js";
 
+// Creating a new instance of the router object using the Router function.
 const router = Router()
 
-router.route("/register").post(registerUser)
-router.route("/login").post(login)
-
+// Defining a POST route at the '/register' path.
+// This route will handle user registration.
+// The 'upload.fields' middleware is applied to this route to handle file uploads for two fields: 
+// After the files are uploaded, the 'registerUser' controller function is executed.
+router.route("/register").post(
+    upload.fields([
+        {name: "avatar", maxCount: 1},  
+        {name: "coberImage", maxCount: 8}  
+    ]),
+    registerUser 
+)
 
 export default router
